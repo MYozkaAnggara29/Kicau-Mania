63677583b825f1f36022debad03acbb0c9933b92
MYozkaAnggara29 <161667691+MYozkaAnggara29@users.noreply.github.com>
2024-03-07 08:47:20 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1709775810,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/flappybird_background-sheet0.png",
+		"images/frame-sheet0.png",
+		"images/frame-sheet1.png",
+		"images/bawahsheet-sheet0.png",
+		"images/atassheet-sheet0.png",
+		"images/batas-sheet0.png",
+		"images/flappybird_gameoverremovebgpreview-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
